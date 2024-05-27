a764ac226124e56e054a46299cc3cf77a0610087
Jaime <87080231+jaimeVillagran@users.noreply.github.com>
2024-05-27 10:42:32 -0400

    first commit

---
+++ src/main/java/cl/project/wallet_personal/WalletPersonal6Application.java
@@ -0,0 +1,13 @@
+package cl.project.wallet_personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletPersonal6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletPersonal6Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/project/wallet_personal/WalletPersonal6ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.project.wallet_personal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletPersonal6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
